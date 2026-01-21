73c046517ef7f6d294798de1184f4e785fc55d68
Igor Khudyakoff <zenex@mail.ru>
2026-01-21 11:51:10 +0300

    Register PaymentReceipt in admin

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Property, Party, LeaseContract, PaymentObligation
+from .models import Property, Party, LeaseContract, PaymentObligation, PaymentReceipt
 
 
 @admin.register(Property)
@@ -28,3 +28,18 @@ class PaymentObligationAdmin(admin.ModelAdmin):
     list_display = ("id", "contract", "period_month", "period_year", "due_date", "amount_due", "status", "responsible_user", "updated_at")
     list_filter = ("status", "period_year", "period_month", "responsible_user")
     search_fields = ("contract__property__name", "contract__tenant__name")
+
+@admin.register(PaymentReceipt)
+class PaymentReceiptAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "obligation",
+        "amount_received",
+        "received_by",
+        "received_at",
+    )
+    list_filter = ("received_by",)
+    search_fields = (
+        "obligation__contract__property__name",
+        "obligation__contract__tenant__name",
+    )
